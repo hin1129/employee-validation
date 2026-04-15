194c548fdb9bfd473a66d7475af56a08bf119444
hin <hin_1129@hotmail.com>
2026-04-15 18:17:16 +0100

    date object

--- src/App.js
+++ src/App.js
@@ -1,22 +1,105 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [name, setName] = useState("")
+  const [email, setEmail] = useState("")
+  const [employeeId, setEmployeeId] = useState("")
+  const [joiningDate, setJoiningDate] = useState("")
+  const today = new Date().toISOString() //2026-04-15T17:15:35.552Z
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+        <input
+          type='text'
+          placeholder='name'
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+
+        {
+          name.length >= 4
+            ?
+            null
+            :
+            <p>min 4 characters</p>
+        }
+
+        <input
+          type='email'
+          placeholder='email'
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+
+        {
+          email.includes("@") && email.includes(".")
+            ?
+            null
+            :
+            <p>not valid email</p>
+        }
+
+        <input
+          type='number'
+          placeholder='employeeId'
+          value={employeeId}
+          onChange={(e) => setEmployeeId(e.target.value)}
+        />
+
+        {
+          employeeId.length === 6
+            ?
+            null
+            :
+            <p>not valid employeeId</p>
+        }
+
+        <input
+          type='date'
+          placeholder='joiningDate'
+          value={joiningDate}
+          onChange={(e) => setJoiningDate(e.target.value)}
+        />
+
+        {today}<br />
+        {today.split("T")}<br />
+        {today.split("T")[0]}<br />
+        {today.split("T")[1]}<br />
+        {Date.now()}<br />
+        {Date.parse(joiningDate)}<br />
+        {joiningDate}
+
+        {
+          // date format is 2026-04-15
+          today.split("T")[0] > joiningDate
+            ?
+            null
+            :
+            <p>joining date cant be in future</p>
+        }
+
+        {/* {
+          // date format is 1776273231442
+          Date.now() > Date.parse(joiningDate)
+            ?
+            null
+            :
+            <p>joining date cant be in future</p>
+        } */}
+
+        <button
+          disabled={
+            !(name.length >= 4) ||
+            !((email.includes("@") && email.includes("."))) ||
+            !(employeeId.length === 6) ||
+            !(today.split("T")[0] > joiningDate)
+          }
         >
-          Learn React
-        </a>
+          submit
+        </button>
       </header>
     </div>
   );
